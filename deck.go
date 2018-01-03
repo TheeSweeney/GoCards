c5009999bb28cbe729f9cbf3d2720c46656827f9
TheeSweeney <bsweeney930@gmail.com>
2018-01-02 23:12:16 -0500

    cleaned up main.go and began using new type deck; created new type deck and gave it print functionality;

--- deck.go
+++ deck.go
@@ -1,1 +1,24 @@
 package main
+
+import "fmt"
+
+//Create a new type of 'deck
+//which is a slice of strings
+
+type deck []string
+
+//though not precisely, the above line can be understood as creating a new type that will extend functionality on top of slices (this is an OO understanding of what is going on)
+
+func (d deck) print() {
+	/*
+		^^^any variable of type "deck" now has access to "print" method
+		(d deck) is referred to as a 'receiver' on a function
+			the first part of the receiver "d" is going to be the actual copy of the deck we're working with as it is available in the function
+				 so in cards.print(), d == cards
+			it serves a similar function to the 'this' keyword in javascript classes
+			by convention, it is not the word 'this', but rather a short abbrev that matches the type of the receiver
+	*/
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- main.go
+++ main.go
@@ -1,46 +1,14 @@
 package main
 
-import "fmt"
-
 func main() {
-	//var card string = "Ace of Spades"
-	//^^^long form method alternatively can be:
-	// card := "Ace of Spades"
-
-	cards := []string{newCard(), "Ace of Diamonds"}
+	// cards := []string{newCard(), "Ace of Diamonds"}
+	// since I've created the deck type, I can ammend the above line to what is below
+	cards := deck{newCard(), "Ace of Diamonds"}
 	cards = append(cards, "Six of Spades")
 
-	/*
-		^^^relying on Go compiler to infer that a var with the val "Ave of Spades" will be a string
-		only use ':=' when assigning a value to a NEW variable
-		after var is initialized it could be reassigned as such
-			car = "Five of Diamonds"
-
-		Variables can be initialized and later assigned
-			var deckSize int
-			deckSize = 53
-		Variables can be initialized outside of a function, but cannot be assigned a value.
-	*/
-
-	/*
-		var - tells go we're creating a variable; card - name of the variable; string - tells compiler that only type string will ever be assigned to this var; Ace of Spades - assign this value to var card
-
-		Go is a statically typed language
-			dynamically typed langauge (JS, Ruby, Python) don't care about the type of a variable
-			statically typed languages (C++, Java, Go) DO care and will create an error if a variable is assigned a value of a type different to what the variable is initially set as
-				main basic data types
-					bool
-					string
-					int - number
-					float64 - number with decimal
-	*/
-
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
-	//go required us to tell it what data type a function will return ^^^
 	return "Five of Diamonds"
 }
