e37fd1f0aac1a36725dde31d431b996358c10fcb
Brian Sweeney <bsweeney930@gmail.com>
2018-01-04 13:43:12 -0500

    added newDeckFromFile functionality;

--- deck.go
+++ deck.go
@@ -1,23 +1,15 @@
 package main
 
-// import "fmt"
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
-//to import multiple librarys, instead of writing multiple lines we just place them in parens (no comma separation) after the initial import
-
-//Create a new type of 'deck
-//which is a slice of strings
-
 type deck []string
 
-//though not precisely, the above line can be understood as creating a new type that will extend functionality on top of slices (this is an OO understanding of what is going on)
-
 func newDeck() deck {
-	//this will not be a receiver function because when this is called we will not already be working with a deck
 	cards := deck{}
 
 	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
@@ -29,31 +21,17 @@ func newDeck() deck {
 		}
 	}
 
-	// "_" is used above because initially the values were "i" and "j", but we never used them
-	// "_" says 'I know there is a variable here, but I really don't care what its value is'
-
 	return cards
 }
 
 func (d deck) print() {
-	/*
-		^^^any variable of type "deck" now has access to "print" method
-		(d deck) is referred to as a 'receiver' on a function
-			the first part of the receiver "d" is going to be the actual copy of the deck we're working with as it is available in the function
-				 so in cards.print(), d == cards
-			it serves a similar function to the 'this' keyword in javascript classes
-			by convention, it is not the word 'this', but rather a short abbrev that matches the type of the receiver
-	*/
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
 
 func deal(d deck, handSize int) (deck, deck) {
-	//parameters must have their type specified
-	//the return values are in parens because we are telling Go that we are returning two values, both of type deck
 	return d[:handSize], d[handSize:]
-	//above, we are returning two separate return values
 }
 
 func (d deck) toString() string {
@@ -62,4 +40,20 @@ func (d deck) toString() string {
 
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666) //0666 permissions mean anyone can read/write this file
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename) //ReadFile returns two values, the contents of a file and the error message, so, we assign the return values to two separate vars
+
+	//if there is no error^^^, it will be assigned the value nil (similar to null)
+	if err != nil {
+		//Option 1 - log error and return a call to newDeck() - if we fail, let's at least return some deck to work with
+		//Option 2 - log error and quit program - let's use this one
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+		//if ^^^ is passed anything but 0, it will exit and close the program
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
--- main.go
+++ main.go
@@ -1,27 +1,8 @@
 package main
 
 func main() {
-	// cards := []string{newCard(), "Ace of Diamonds"}
-	// since I've created the deck type, I can ammend the above line to what is below
-	//cards := deck{newCard(), "Ace of Diamonds"}
-	//and now that I wrote a newDeck() function, I can again rewrite it as:
-	cards := newDeck()
-
-	// hand, remainingDeck := deal(cards, 5)
-	// //deal returns 2 seperate values
-	// //the first will be assigned to var "hand", the second to var "remainingDeck"
-
-	// hand.print()
-	// remainingDeck.print()
-	//both 'hand' and 'remainingDeck' are of type 'deck' and so have access to the print function
-	// greeting := "Hi there!"
-	// fmt.Println(greeting)
-	// >"Hi there!"
-	// byteGreet := []byte(greeting)
-	// fmt.Println(byteGreet)
-	// >[72 105 32 116 104 101 114 101 33]
-
-	cards.saveToFile("my_cards")
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 
 }
 
