8962849916e1731895369c672f4533b9c6a5e9ca
Brian Sweeney <bsweeney930@gmail.com>
2018-01-04 13:20:32 -0500

    implemented saveToFile functionality as well as a toString function for type decks;

--- deck.go
+++ deck.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+// import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+//to import multiple librarys, instead of writing multiple lines we just place them in parens (no comma separation) after the initial import
 
 //Create a new type of 'deck
 //which is a slice of strings
@@ -18,7 +25,7 @@ func newDeck() deck {
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
-			cards = append(cards, value + "of" + suit)
+			cards = append(cards, value+"of"+suit)
 		}
 	}
 
@@ -42,10 +49,17 @@ func (d deck) print() {
 	}
 }
 
-
 func deal(d deck, handSize int) (deck, deck) {
 	//parameters must have their type specified
 	//the return values are in parens because we are telling Go that we are returning two values, both of type deck
 	return d[:handSize], d[handSize:]
 	//above, we are returning two separate return values
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666) //0666 permissions mean anyone can read/write this file
 }
--- main.go
+++ main.go
@@ -7,13 +7,22 @@ func main() {
 	//and now that I wrote a newDeck() function, I can again rewrite it as:
 	cards := newDeck()
 
-	hand, remainingDeck := deal(cards, 5)
-	//deal returns 2 seperate values
-	//the first will be assigned to var "hand", the second to var "remainingDeck"
+	// hand, remainingDeck := deal(cards, 5)
+	// //deal returns 2 seperate values
+	// //the first will be assigned to var "hand", the second to var "remainingDeck"
 
-	hand.print()
-	remainingDeck.print()
+	// hand.print()
+	// remainingDeck.print()
 	//both 'hand' and 'remainingDeck' are of type 'deck' and so have access to the print function
+	// greeting := "Hi there!"
+	// fmt.Println(greeting)
+	// >"Hi there!"
+	// byteGreet := []byte(greeting)
+	// fmt.Println(byteGreet)
+	// >[72 105 32 116 104 101 114 101 33]
+
+	cards.saveToFile("my_cards")
+
 }
 
 func newCard() string {
